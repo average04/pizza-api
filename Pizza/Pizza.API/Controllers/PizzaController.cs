885660e40f35ee11e7b407322068c53a60d9b6ba
DESKTOP-690PG9R\average <bayogjayr@gmail.com>
2024-08-10 23:25:49 +0800

    Updated : OrderController to PizzaContoller

---
+++ Pizza/Pizza.API/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿namespace Pizza.API.Controllers;
+
+public class PizzaController : BaseController
+{
+    [HttpPost("upload-pizza-csv")]
+    public async Task<IActionResult> UploadPizzaCsv([FromForm] IFormFile file)
+    {
+        var request = new UploadPizzaRequest(file);
+        await Mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPost("upload-pizza-type-csv")]
+    public async Task<IActionResult> UploadPizzaTypeCsv([FromForm] IFormFile file)
+    {
+        var request = new UploadPizzaTypeRequest(file);
+        await Mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPost("upload-order-csv")]
+    public async Task<IActionResult> UploadOrderCsv([FromForm] IFormFile file)
+    {
+        var request = new UploadOrderRequest(file);
+        await Mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPost("upload-order-detail-csv")]
+    public async Task<IActionResult> UploadOrderDetailCsv([FromForm] IFormFile file)
+    {
+        var request = new UploadOrderDetailRequest(file);
+        await Mediator.Send(request);
+        return Ok();
+    }
+}
+
